5304fd7b8ebda3fcbb5cae9d2957e0adbbb9b5d0
Ruben <siadiui@gmail.com>
2016-09-14 21:31:37 +0200

    remove query string for js and css files

--- remove-emojis-meta.php
+++ remove-emojis-meta.php
@@ -59,3 +59,17 @@ function disable_emojis_tinymce( $plugins ) {
 		return array();
 	}
 }
+
+/**
+ * Resources with a "?" in the URL are not cached by some proxy caching servers.
+ * This removes the query string for css and js files
+ */
+function remove_cssjs_ver( $src ) {
+    if( strpos( $src, '?ver=' ) ) {
+        $src = remove_query_arg( 'ver', $src );
+    }
+    return $src;
+}
+
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
